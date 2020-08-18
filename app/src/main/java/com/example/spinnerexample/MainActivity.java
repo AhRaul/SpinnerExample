8b16394da4b8d0cc2a8badf8924279cf86973ba3
AhRaul <ralchik@yandex.ru>
2020-08-18 16:37:35 +0300

    Click

--- app/src/main/java/com/example/spinnerexample/MainActivity.java
+++ app/src/main/java/com/example/spinnerexample/MainActivity.java
@@ -3,17 +3,26 @@ package com.example.spinnerexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] cities = {"Москва", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж"};
+    String[] cities = {"Москва", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж",
+            "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж",
+            "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж",
+            "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж"};
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = (TextView)findViewById(R.id.textView);
         Spinner spinner = (Spinner) findViewById(R.id.spinner_list);
         // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cities);
@@ -21,5 +30,20 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Применяем адаптер к элементу spinner
         spinner.setAdapter(adapter);
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Получаем выбранный обьект
+                String item = (String) parent.getItemAtPosition(position);
+                textView.setText(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+        spinner.setOnItemSelectedListener(itemSelectedListener);
     }
 }
